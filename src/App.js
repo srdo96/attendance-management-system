ab7ab17a15fd9e42ffd53e538f1fc90ce151c423
Sakib Rahman <seu.sakib@gmail.com>
2022-08-14 16:24:53 +0600

    add signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
       </Routes>
     </div>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,19 @@
 import React from "react";
 import CustomButton from "../../components/CustomButton/CustomButton";
 import CustomInput from "../../components/CustomInput/CustomInput";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <CustomInput type="text" placeholder="EMAIL" />
-      <br />
-      <CustomInput type="password" placeholder="PASSWORD" />
-      <br />
-      <CustomButton label="Login" />
+    <div className="login-card">
+      <div className="card">
+        <h1>Login</h1>
+        <CustomInput type="text" placeholder="EMAIL" />
+        <br />
+        <CustomInput type="password" placeholder="PASSWORD" />
+        <br />
+        <CustomButton label="Login" />
+      </div>
     </div>
   );
 };
